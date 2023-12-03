9813f3721af4f295adb91db416f45dc2e45c1f1e
JeanIfsp <126697698+JeanIfsp@users.noreply.github.com>
2023-12-03 11:39:10 -0300

    Improvement function producer and consumer

--- consumer.c
+++ consumer.c
@@ -3,18 +3,12 @@
 #include <pthread.h>
 #include<unistd.h>
 #include<time.h>
-
-
-
 #define BUFFER_SIZE 5
 
 pthread_cond_t free_espace, full_space;
 pthread_mutex_t block;
 
 int buffer[BUFFER_SIZE];
-int position_read = 0;
-int position_write = 0;
-int position_insert = 0;
 int counter = 0;
 
 int numCons;
@@ -28,30 +22,6 @@ int generatePosotiveNumber(){
   return rand() % 100 + 1;
 }
 
-int readBuffer(){
-
-  int item = buffer[position_read];
-  position_read++;
-
-  if(position_read == BUFFER_SIZE){
-    position_read = 0;
-  }
-  counter--;
-  return item;
-
-}
-
-void insertBuffer(int value, int producerNumber){
-
-    buffer[position_insert] = value;
-    printf("Thread  %d (value: %d)\n", producerNumber, value);
-    position_write++;
-    if(position_write == BUFFER_SIZE) {
-      position_insert = 0;
-    }
-    counter++;
-}
-
 void* producer(void *arg) {
     while(1){
       int producerNumber = *((int *)arg);  
@@ -59,22 +29,21 @@ void* producer(void *arg) {
       if(counter < BUFFER_SIZE){
         int number = generatePosotiveNumber();
         buffer[counter] = number;
-        printf("Thread producer %d (value: %d)\n", producerNumber, number);
+        printf("Thread %d produziu valor %d\n", producerNumber, number);
         counter++;
       }
       pthread_mutex_unlock(&block);
   }
 }
 
-
 void* consumer(void *arg){
 
     while(1){
       int producerNumber = *((int *)arg);  
       pthread_mutex_lock(&block);
-      if(counter == 0){
+      if(counter == BUFFER_SIZE){
         int number = buffer[counter - 1];
-        printf("Thread consumer %d (value: %d)\n", producerNumber, number);
+        printf("Thread %d consumiu valor %d\n", producerNumber, number);
         counter--;
       }
     pthread_mutex_unlock(&block);
@@ -83,23 +52,25 @@ void* consumer(void *arg){
 
 int main(int argc,char *argv[]){
 
-    pthread_t thread_prod, thread_cons;
-    int id;
+    pthread_t thread[2];
+    int id, success;
 
     if (argc != 3) {
         printf("Modo de utilizar: ./consumer <no produtores> <no produtores>");
-    }else{
+    }
+    else{
+      
       numProd = atoi(argv[1]);
       numCons = atoi(argv[2]);
       
       if(numCons >= 1 && numProd >= 1){
         
         for(id = 0; id < numProd; id++){
-          pthread_create(&thread_prod, NULL, producer, (void *)&id);
+           success = pthread_create(&thread[id], NULL, producer, (void *)&id);
         }
         
         for(id = 0; id < numCons; id++){
-          pthread_create(&thread_cons, NULL, consumer, (void *)&id);
+           success = pthread_create(&thread[id], NULL, consumer, (void *)&id);
         }
 
       }else{
@@ -107,4 +78,5 @@ int main(int argc,char *argv[]){
         exit(0);
       }
     }
+    pthread_exit(NULL);
 }
